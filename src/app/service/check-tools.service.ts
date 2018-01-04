2c8d33df12ca624a7d43cd54c5a5e9aee395c2b6
Antoine Stalin <astalin@norsys.fr>
2018-01-04 21:02:39 +0100

    fix(check) : type response for http client

--- src/app/service/check-tools.service.ts
+++ src/app/service/check-tools.service.ts
@@ -17,18 +17,20 @@ export class CheckToolsService {
 
 
   health(tools: string, identity : Identity): Observable<Boolean> {
-    return this._http.get(`http://${tools}.${identity.ciDomain}/login`)
+    return this._http.get(`http://${tools}.${identity.ciDomain}/login`.toString(),  {responseType: 'text'})
       .map(res => this.extractData(res, tools, identity))
       .catch(res => this.handleError(res, tools, identity));
   }
 
   private extractData(res: Response | any, tools: string,identity : Identity) {
     identity.checkTools[tools] = true;
+    console.log('ok');
     return true;
   }
 
   private handleError(error: Response | any, tools: string,identity : Identity) {
     identity.checkTools[tools] = false;
+    console.log('fail : '+JSON.stringify(error));
     return Observable.throw(false);
   }
 }
